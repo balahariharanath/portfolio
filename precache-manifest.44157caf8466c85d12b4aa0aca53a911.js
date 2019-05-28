d7c8390315729910a6a6630700dc546ed8534740
balahariharanath <38852515+balahariharanath@users.noreply.github.com>
2019-05-28 22:05:31 +0530

    Updates

---
+++ precache-manifest.44157caf8466c85d12b4aa0aca53a911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8419970133ea58dd152",
+    "url": "/portfolio/static/css/main.b2969665.chunk.css"
+  },
+  {
+    "revision": "d8419970133ea58dd152",
+    "url": "/portfolio/static/js/main.d8419970.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "abd677ede567b7f416ffbd7360f3043a",
+    "url": "/portfolio/index.html"
+  }
+];
